c2e8c086a6de92eef43c8682ef0b60f05326a4e9
goodgd <dionatha.work@gmail.com>
2025-12-17 15:03:33 -0300

    fix: build

--- apps/portfolio/src/app/creepy/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { Creepy } from '@/views/Projetos/Creepy'
-
-export default function CreepyPage() {
-  return <Creepy />
-}
-
--- apps/portfolio/src/app/halloween/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { Halloween } from '@/views/Projetos/Halloween'
-
-export default function HalloweenPage() {
-  return <Halloween />
-}
-
